82855e33b8371a18a40e2109e0f941a7c503d3e1
KsushaTarasova <ksutaras543@mail.ru>
2024-10-12 14:06:48 +0300

    Add setup method with @BeforeEach annotation

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,5 +1,13 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class CalculatorTest {
+    private Calculator calculator;
+
+    @BeforeEach
+    public void setup() {
+        calculator = new Calculator();
+    }
 
 }
