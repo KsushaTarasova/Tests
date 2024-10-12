d280e3a9813099658d5c56abaf1aef209160de56
KsushaTarasova <ksutaras543@mail.ru>
2024-10-12 14:04:23 +0300

    Add Calculator class

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException("Деление на 0");
+        }
+        return (double) a / b;
+    }
+
+    public int modulus(int a, int b) {
+        return a % b;
+    }
+}
