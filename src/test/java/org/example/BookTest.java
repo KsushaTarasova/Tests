af4329b0c130ba6de8798a9de6ef7dbe5eebb8ba
KsushaTarasova <ksutaras543@mail.ru>
2024-10-12 14:26:55 +0300

    Add testGetTitle method

--- src/test/java/org/example/BookTest.java
+++ src/test/java/org/example/BookTest.java
@@ -1,4 +1,13 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BookTest {
+    @Test
+    public void testGetTitle(){
+        Book book = new Book("The Hobbit", "J.R.R. Tolkien", 310);
+        assertEquals("The Hobbit", book.getTitle());
+    }
 }
