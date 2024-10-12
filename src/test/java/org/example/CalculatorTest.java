7de8fdff181c7270683210c4f0e352d29881e281
KsushaTarasova <ksutaras543@mail.ru>
2024-10-12 14:20:36 +0300

    Add testSubtract method

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -16,6 +16,8 @@ public class CalculatorTest {
     public void testAdd() {
         assertEquals(15, calculator.add(8, 7));
     }
-
-
+    @Test
+    public void testSubtract() {
+        assertEquals(2, calculator.subtract(20, 18));
+    }
 }
