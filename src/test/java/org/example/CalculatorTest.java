7409ad26ab212c6afe8beab3048958af621aec00
KsushaTarasova <ksutaras543@mail.ru>
2024-10-20 14:20:40 +0300

    Add "unique" tests (((

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -9,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CalculatorTest {
     private Calculator calculator;
+    private int a = 0x141D12;
 
     @BeforeEach
     public void setup() {
@@ -16,29 +17,29 @@ public class CalculatorTest {
     }
     @Test
     public void testAdd() {
-        assertEquals(15, calculator.add(8, 7));
+        assertEquals(15, calculator.add(8, Month.JULY.getNumber()));
     }
     @Test
     public void testSubtract() {
+        assertEquals(18.2, calculator.subtract(20, 1.8f), 0.001);
         assertEquals(2, calculator.subtract(20, 18));
     }
     @Test
     public void testMultiply() {
-        assertEquals(40, calculator.multiply(8, 5));
+        assertEquals(40, calculator.multiply(8, Integer.parseInt("5")));
     }
     @Test
     public void testDivide() {
-        assertEquals(7, calculator.divide(28, 4));
+        assertEquals(7, calculator.divide(28, a));
     }
     @Test
     public void testDivideByZero() {
-        assertThrows(ArithmeticException.class, () -> {
-            calculator.divide(100, 0);
-        }, "ArithmeticException was expected");
+        assertThrows(ArithmeticException.class, () -> calculator.divide(100, 0),
+                "ArithmeticException was expected");
     }
     @Test
     public void testModulus(){
-        assertEquals(10, calculator.modulus(210, 20));
+        assertEquals(10, calculator.modulus(210, "qwerty".length()));
     }
     @AfterEach
     public void print() {
